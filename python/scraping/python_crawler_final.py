e20d56ade6893a1ba534dfffb7f546256ddcdd9a
KeiKawano <k.kawano@sint.co.jp>
2017-04-06 22:47:33 +0900

    create crawler final

--- python/scraping/python_crawler_final.py
+++ python/scraping/python_crawler_final.py
@@ -1,20 +1,32 @@
 import time
 import re
+
 import requests
 import lxml.html
+from pymongo import MongoClient
 
 def main():
   """
   クローラーのメインの処理
   """
+  client = MongoClient('localhost', 27017) # ローカルホストのMongoDBに接続する
+  collection = client.scraping.ebooks # scrapingデータベースのebooksコレクションを得る
+  # データを一意に識別するキーを格納するkeyフィールドにユニークなインデックスを作成する
+  collection.create_index('key', unique=True)
   session = requests.Session() # 複数のページをスクロールするのでSessionを使う
-  response = session.get('https://gihyo.jp/dp')
-  urls = scrape_list_page(response)
+  response = session.get('https://gihyo.jp/dp') # 一覧ページを取得する
+  urls = scrape_list_page(response) # 詳細ページのURL一覧を得る
   for url in urls: # ジェネレーターイテレーターはlistなどと同様に繰り返し可能
-    time.sleep(1) # 1秒のウェイトを入れる
-    response = session.get(url) # Sessionを使って詳細ページを取得する
-    ebook = scrape_detail_page(response) # 詳細ページからスクレイピングして電子書籍の情報を得る
-    print(ebook)
+    key = extract_key(url) # URLからキーを取得する
+    ebook = collection.find_one({'key': key}) # MongoDBからkeyに該当するデータを探す
+    if not ebook: # MongoDBに存在しない場合だけ、詳細ページをスクロールする
+      time.sleep(1) # 1秒のウェイトを入れる
+      response = requests.get(url)
+      ebook = scrape_detail_page(response) # 詳細ページからスクレイピングして電子書籍の情報を得る
+      print(ebook)
+      collection.insert_one(ebook) # 電子書籍の情報をMongoDBに保存する
+    
+    print(ebook) # 電子書籍の情報を表示
 
 def scrape_list_page(response):
   """
@@ -25,7 +37,7 @@ def scrape_list_page(response):
 
   for a in root.cssselect('#listBook a[itemprop="url"]'):
     url = a.get('href')
-    yield url # 
+    yield url
 
 def scrape_detail_page(response):
   """
@@ -34,11 +46,20 @@ def scrape_detail_page(response):
   root = lxml.html.fromstring(response.content)
   ebook = {
     'url': response.url, # URL
+    'key': extract_key(response.url), # URLから抜き出したキー
     'title': root.cssselect('#bookTitle')[0].text_content(), # タイトル
     'price': root.cssselect('.buy')[0].text.strip(), # 価格（.textで直接の子である文字列のみを取得）
     'content': [normalize_spaces(h3.text_content()) for h3 in root.cssselect('#content > h3')], # 目次
   }
   return ebook # dictを返す
+
+def extract_key(url):
+  """
+  URLからキー（URLの末尾のISBN）を抜き出す
+  """
+  m = re.search(r'/([^/]+)$', url)
+  return m.group(1)
+  
   
 def normalize_spaces(s):
   """
