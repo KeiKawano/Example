38634007c1b5f1baef45ce56fac52c393b676187
KeiKawano <k.kawano@sint.co.jp>
2017-04-06 21:51:57 +0900

    create example for crawler

---
+++ python/scraping/python_crawler_3.py
@@ -0,0 +1,26 @@
+import requests
+import lxml.html
+
+def main():
+  """
+  クローラーのメインの処理
+  """
+  response = requests.get('https://gihyo.jp/dp')
+  # scrape_list_page()を呼出し、ジェネレーターイテレーターを取得する
+  urls = scrape_list_page(response)
+  for url in urls: # ジェネレーターイテレーターはlistなどと同様に繰り返し可能
+    print(url)
+
+def scrape_list_page(response):
+  """
+  一覧ページのResponseから詳細ページのURLを抜き出すジェネレーター関数
+  """
+  root = lxml.html.fromstring(response.content)
+  root.make_links_absolute(response.url)
+
+  for a in root.cssselect('#listBook a[itemprop="url"]'):
+    url = a.get('href')
+    yield url # 
+
+if __name__ == '__main__':
+  main()
